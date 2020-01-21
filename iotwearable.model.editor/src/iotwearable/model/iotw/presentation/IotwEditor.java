3234b471995ff5e001ba43dffaa267b0f2fababa
nnthuan <nguyennhuthuanse@gmail.com>
2020-01-21 23:08:54 +0700

    Resolve warning,  change version

--- iotwearable.model.editor/src/iotwearable/model/iotw/presentation/IotwEditor.java
+++ iotwearable.model.editor/src/iotwearable/model/iotw/presentation/IotwEditor.java
@@ -1290,7 +1290,6 @@ public class IotwEditor
 	 * <!-- end-user-doc -->
 	 * @generated
 	 */
-	@SuppressWarnings("rawtypes")
 	@Override
 	public <T> T getAdapter(Class<T> key) {
 		if (key.equals(IContentOutlinePage.class)) {
